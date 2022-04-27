91991295469ab3568d77d6924e1d335099984902
seungyeon.lee <dus3214@gmail.com>
2022-04-27 22:14:02 +0900

    add: 문제풀이추가

---
+++ programmers/폰켓몬.js
@@ -0,0 +1,13 @@
+// https://programmers.co.kr/learn/courses/30/lessons/1845
+// 당신은 최대한 다양한 종류의 폰켓몬을 가지길 원하기 때문에, 최대한 많은 종류의 폰켓몬을 포함해서 N/2마리를 선택하려 합니다. N마리 폰켓몬의 종류 번호가 담긴 배열 nums가 매개변수로 주어질 때, N/2마리의 폰켓몬을 선택하는 방법 중, 가장 많은 종류의 폰켓몬을 선택하는 방법을 찾아, 그때의 폰켓몬 종류 번호의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+function solution(nums) {
+  let answer = [...new Set(nums)];
+  const len = nums.length / 2;
+
+  return answer.length > len ? len : answer.length;
+}
+
+console.log(solution([3, 1, 2, 3]));
+console.log(solution([3, 3, 3, 2, 2, 4]));
+console.log(solution([3, 3, 3, 2, 2, 2]));
